0f0c6d4fa1f112a810dc442b0294dafabd3226df
Rafael Menezes <rafa.rcm@hotmail.com>
2023-05-26 20:54:19 -0300

    Add project files.

---
+++ WindowsForms/Form1.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
