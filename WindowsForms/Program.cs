f047f0b625279ab61f3d927ec72875beb4b8d163
Rafael Menezes <rafa.rcm@hotmail.com>
2023-05-28 10:16:18 -0300

    Paramentros de um envento

--- WindowsForms/Program.cs
+++ WindowsForms/Program.cs
@@ -13,7 +13,7 @@ namespace WindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_HelloWorld());
+            Application.Run(new Frm_DesmonstracaoKey());
         }
     }
 }
