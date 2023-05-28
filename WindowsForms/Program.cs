a46639f264b6d245d5c4cd2d024c8c9bca943898
Rafael Menezes <rafa.rcm@hotmail.com>
2023-05-28 10:51:33 -0300

    Form Validar Senha

--- WindowsForms/Program.cs
+++ WindowsForms/Program.cs
@@ -13,7 +13,7 @@ namespace WindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_DesmonstracaoKey());
+            Application.Run(new Frm_ValidaSenha());
         }
     }
 }
