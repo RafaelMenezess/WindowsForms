bae8cd06218fab00aa1c6e74f88c26c192b753f9
Rafael Menezes <rafa.rcm@hotmail.com>
2023-05-28 15:31:18 -0300

    Criando Frm e manipulando máscaras

--- WindowsForms/Program.cs
+++ WindowsForms/Program.cs
@@ -13,7 +13,7 @@ namespace WindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_ValidaSenha());
+            Application.Run(new Frm_Mascara());
         }
     }
 }
