934dea69f88bf5fe941b879aa5db1285ac78d442
Rafael Menezes <rafa.rcm@hotmail.com>
2023-05-31 20:55:46 -0300

    Caixa de Mensagem

--- WindowsForms/Frm_ValidaCPF2.cs
+++ WindowsForms/Frm_ValidaCPF2.cs
@@ -16,5 +16,25 @@ namespace WindowsForms
         {
             InitializeComponent();
         }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = string.Empty;
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            bool validaCpf = false;
+            validaCpf = Cls_Uteis.Valida(Msk_CPF.Text);
+
+            if (validaCpf == true)
+            {
+                MessageBox.Show("CPF Válido", "Mensagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("CPF Inválido", "Mensagem de validação", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
     }
 }
--- WindowsForms/Program.cs
+++ WindowsForms/Program.cs
@@ -13,7 +13,7 @@ namespace WindowsForms
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Frm_ValidaCPF());
+            Application.Run(new Frm_ValidaCPF2());
         }
     }
 }
