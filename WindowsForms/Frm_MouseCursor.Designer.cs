1959981ecf52586ae9a1b3aa1a229c940ca57d48
Rafael Menezes <rafa.rcm@hotmail.com>
2023-06-12 21:29:51 -0300

    Cursor

---
+++ WindowsForms/Frm_MouseCursor.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsForms
+{
+    partial class Frm_MouseCursor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Btn
+            // 
+            this.Btn.Location = new System.Drawing.Point(79, 69);
+            this.Btn.Name = "Btn";
+            this.Btn.Size = new System.Drawing.Size(244, 122);
+            this.Btn.TabIndex = 0;
+            this.Btn.Text = "button1";
+            this.Btn.UseVisualStyleBackColor = true;
+            this.Btn.Click += new System.EventHandler(this.Btn_Click);
+            // 
+            // Frm_MouseCursor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 270);
+            this.Controls.Add(this.Btn);
+            this.Name = "Frm_MouseCursor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frm_MouseCursor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn;
+    }
+}
