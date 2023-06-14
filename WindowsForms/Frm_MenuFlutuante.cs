c030c4efbea53ea4fb1d5f3c96dc35f73ee85d10
Rafael Menezes <rafa.rcm@hotmail.com>
2023-06-13 21:38:57 -0300

    Programando as ações do menu

--- WindowsForms/Frm_MenuFlutuante.cs
+++ WindowsForms/Frm_MenuFlutuante.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace WindowsForms
@@ -15,19 +16,33 @@ namespace WindowsForms
             if (e.Button == MouseButtons.Right)
             {
                 var contextMenu = new ContextMenuStrip();
-                contextMenu.Items.Add(DesenhaItemMenu("Item do menu 1", "icons8_key_96"));
-                contextMenu.Items.Add(DesenhaItemMenu("Item do menu 2", "Frm_ValidaSenha"));
+                var vTooltip001 = DesenhaItemMenu("Item do menu 1", "icons8_key_96");
+                var vTooltip002 = DesenhaItemMenu("Item do menu 2", "Frm_ValidaSenha");
+
+                contextMenu.Items.Add(vTooltip001);
+                contextMenu.Items.Add(vTooltip002);
                 contextMenu.Show(this, new Point(e.X, e.Y));
+
+                vTooltip001.Click += new EventHandler(vTooltip001_Click);
+                vTooltip002.Click += new EventHandler(vTooltip002_Click);
             }
         }
         private ToolStripMenuItem DesenhaItemMenu(string text, string nomeImagem)
         {
-            var vToolti = new ToolStripMenuItem();
-            vToolti.Text = text;            
+            var vTooltip = new ToolStripMenuItem();
+            vTooltip.Text = text;            
             Image myImage = (Image)Properties.Resources.ResourceManager.GetObject(nomeImagem);
-            vToolti.Image = myImage;
+            vTooltip.Image = myImage;
 
-            return vToolti;
+            return vTooltip;
+        }
+        private void vTooltip001_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selecionei opção do menu 001");
+        }
+        private void vTooltip002_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selecionei opção do menu 002");
         }
     }
 }
