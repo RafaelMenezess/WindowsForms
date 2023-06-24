9680d7f8f08499fdf79f69689352773723ae9b7d
Rafael Menezes <rafa.rcm@hotmail.com>
2023-06-24 15:35:11 -0300

    Criando a classe fichario

---
+++ CursoWindownsFormsBiblioteca/DataBases/Fichario.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace CursoWindownsFormsBiblioteca.DataBases
+{
+    public class Fichario
+    {
+        public string diretorio;
+        public string mensagem { get; set; }
+        public bool status { get; set; }
+        public Fichario(string Diretorio)
+        {
+            status = true;
+            try
+            {
+                if (!(Directory.Exists(Diretorio)))
+                {
+                    Directory.CreateDirectory(Diretorio);
+                }
+                diretorio = Diretorio;
+                mensagem = "Conexão com o Fichario criada com sucesso.";
+
+            }
+            catch (Exception ex)
+            {
+                status = false;
+                mensagem = "Conexão com o Fichario com erro: " + ex.Message;
+                throw;
+            }
+
+        }
+
+    }
+}
