6ae7d637e2d528d6f1fa4088f4c174aff21173d0
Rafael Menezes <rafa.rcm@hotmail.com>
2023-07-10 21:23:29 -0300

    Criando a classe de acesso ao banco de dados

---
+++ CursoWindownsFormsBiblioteca/DataBases/LocalDBClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+namespace CursoWindownsFormsBiblioteca.DataBases
+{
+    public class LocalDBClass
+    {
+        public string stringConn;
+        public SqlConnection connDb;
+
+        public LocalDBClass()
+        {
+            stringConn = "";
+            connDb = new SqlConnection(stringConn);
+            connDb.Open();
+        }
+    }
+}
