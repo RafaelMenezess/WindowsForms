963628dacd67054679257f55d7189774d1599978
Rafael Menezes <rafa.rcm@hotmail.com>
2024-03-28 15:25:27 -0300

    Manipulando a tabela de agências

--- WindowsForms/Frm_Agencia.cs
+++ WindowsForms/Frm_Agencia.cs
@@ -15,5 +15,20 @@ namespace WindowsForms
         {
             this.Close();
         }
+
+        private void tB_AgenciaBindingNavigatorSaveItem_Click(object sender, System.EventArgs e)
+        {
+            this.Validate();
+            this.tB_AgenciaBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.byteBankDataSet);
+
+        }
+
+        private void Frm_Agencia_Load(object sender, System.EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'byteBankDataSet.TB_Agencia' table. You can move, or remove it, as needed.
+            this.tB_AgenciaTableAdapter.Fill(this.byteBankDataSet.TB_Agencia);
+
+        }
     }
 }
--- WindowsForms/Properties/Settings.Designer.cs
+++ WindowsForms/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace WindowsForms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,17 @@ namespace WindowsForms.Properties {
         public string FicharioConnectionString {
             get {
                 return ((string)(this["FicharioConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-1AMLFGM;Initial Catalog=ByteBank;Persist Security Info=True;U" +
+            "ser ID=sa;Password=123")]
+        public string ByteBankConnectionString {
+            get {
+                return ((string)(this["ByteBankConnectionString"]));
             }
         }
     }
